621f9747daa36ad9a78d9513f7dcdc7bc1880206
Matthias Monden <matthias@Matthias.MatthiasKALI>
2020-11-10 16:04:50 +0100

    update WPA2.py

--- WPA2.py
+++ WPA2.py
@@ -4,11 +4,9 @@ from SubBytesInv import *
 from ShiftRows import *
 
 """
-the matrix has to be a square matrix with hex value
+	the matrix has to be a square matrix with hex value
 
 """
-
-
 def xor(matrix1, matrix2):
 	new_matrix = make_empty_matrix(nb_rows=4)
 
@@ -18,14 +16,14 @@ def xor(matrix1, matrix2):
 
 	return new_matrix
 
-
-# the key and decrypted_message are a list in a list structure
-# round_keys is a dictionary where the keys are roundi with i the specific round and the values the round key
-# round0 is the orginal key
-
-
+"""
+	the key and decrypted_message are a list in a list structure
+	round_keys is a dictionary where the keys are roundi with i the specific round and the values the round key
+	round0 is the orginal key
+"""
 def decrypt_wpa2_data(encrypted_message, key, rounds=10):
 	round_keys = make_key_streams(key, rounds)
+
 	for i in range(rounds, -1, -1):
 		# i = 10 tot en met 0
 		if i == 10:
@@ -73,11 +71,9 @@ def key_to_matrix(key):
 
 	return new_matrix
 
+key = [[0x2b, 0x28, 0xab, 0x09], [0x7e, 0xae, 0xf7, 0xcf], [0x15, 0xd2, 0x15, 0x4f], [0x16, 0xa6, 0x88, 0x3c]]
+encrypted_message = [[0x39, 0x02, 0xdc, 0x19], [0x25, 0xdc, 0x11, 0x6a], [0x84, 0x09, 0x85, 0x0b], [0x1d, 0xfb, 0x97, 0x32]]
 
-
-key = str(0x2b) + str(0x28) + str(0xab) + str(0x09) + str(0x7e) + str(0xae) + str(0xf7) + str(0xcf) + str(0x15) + str(0xd2) + str(0x15) + str(0x4f) + str(0x16) + str(0xa6) + str(0x88) + str(0x3c) #[[0x2b, 0x28, 0xab, 0x09], [0x7e, 0xae, 0xf7, 0xcf], [0x15, 0xd2, 0x15, 0x4f], [0x16, 0xa6, 0x88, 0x3c]]
-encrypted_message = [[0x39, 0x02, 0xdc, 0x19], [0x25, 0xdc, 0x11, 0x6a], [0x84, 0x09, 0x85, 0x0b],
-					 [0x1d, 0xfb, 0x97, 0x32]]
+print(key)
 
 print(decrypt_wpa2_data(encrypted_message,key))
-
