700de3271358e3c8f06d27f367a374a5474aacab
JDogM <48888533+JDogM@users.noreply.github.com>
2020-12-08 11:00:34 +0100

    Rename AirodumpWPA2.py to WPA2FIN/AirodumpWPA2.py

--- AirodumpWPA2.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-mac = sys.argv[1]
-filepath = sys.argv[2]
-
-os.system("sudo airodump-ng --bssid {} ".format(mac) + " -c 1 -w " + filepath + "/WPA_{} ".format(mac) + "--output-format cap wlan1mon")
